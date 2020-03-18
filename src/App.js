96c228450486753f3a9040c61a8d261b8a28e8b8
Manuel Ortega Carcamo <mortega@apgca.cl>
2020-03-18 00:18:22 -0300

    chore: Utilizacion de context para aplicar temas en la app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         React Context API
-        </p>
-       
-      </header>
-    </div>
-  );
+import * as themes from "./themes";
+import ThemeContext from "./themes/context";
+
+class App extends Component {
+  state = {
+    theme: themes.dark
+  };
+
+  toggleTheme = () => {
+      this.setState({
+        theme: this.state.theme === themes.dark ? themes.light : themes.dark
+      });
+  };
+
+  render(){
+    const {theme} = this.state;
+    return (
+      <>
+        <ThemeContext.Provider value={theme}>
+
+                <div className="App">
+                  <header 
+                    className="App-header"
+                    style={{
+                      backgroundColor: this.state.theme.background,
+                      color: this.state.theme.color
+                    }}
+                  
+                  >
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>React Context API</p>
+                    <code style={{color: 'gray', fontSize: 14}}>Tema: {JSON.stringify(this.state)}</code>
+                    <br />
+                    <button 
+                      onClick={this.toggleTheme}
+                      style={{ 
+                          backgroundColor: this.state.theme.background,
+                          color: this.state.theme.color
+                        }}
+                    >Cambiar tema</button>
+
+                  </header>
+                </div>
+
+         </ThemeContext.Provider>
+      </>
+    );
+  }
+
+
 }
 
 export default App;
